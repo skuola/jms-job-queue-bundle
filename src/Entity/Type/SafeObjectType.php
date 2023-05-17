7c8b9e0bda8ceaabd42cf26a767ab37f48bd2f1e
stefanofranco <stefano.franco1@gmail.com>
2023-05-17 16:08:20 +0200

    First Commit

---
+++ src/Entity/Type/SafeObjectType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JMS\JobQueueBundle\Entity\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ObjectType;
+
+class SafeObjectType extends ObjectType
+{
+    public function getSQLDeclaration(array $fieldDeclaration, \Doctrine\DBAL\Platforms\AbstractPlatform $platform)
+    {
+        return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    public function getName()
+    {
+        return 'jms_job_safe_object';
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+
+
+}
